32d0adf868bcb3a6c82ff733448d234dd3720258
Grif-Real <70777013+Grif-Real@users.noreply.github.com>
2021-12-15 11:50:44 +0100

    Added the subtractStringArrays function

---
+++ features/arrays/subtractStringArrays.js
@@ -0,0 +1,30 @@
+function subtractStringArrays(firstArray, secondArray) {
+
+    if (!firstArray || !secondArray) throw new Error('You must provide two arrays for the "subtractStringArrays" function.')
+    if (!Array.isArray(firstArray) || !Array.isArray(secondArray)) throw new Error(`One or two of the arguments provided for the "subtractStringArrays" function aren't an Array.`)
+
+    for (const data of firstArray) {
+        if (typeof data != 'string') throw new Error('The first specified array for the "subtractStringArrays" function, contains a variable that is not a string.')
+    }
+
+    for (const data of secondArray) {
+        if (typeof data != 'string') throw new Error('The second specified array for the "subtractStringArrays" function, contains a variable that is not a string.')
+    }
+
+    for (const item of firstArray) {
+
+        if (secondArray.includes(item)) {
+
+            secondArray.splice(secondArray.indexOf(item), 1) // Splice removes one element from the Array, we use Array.indexOf(item) to return the number of the item needed to be removed in the array.
+            firstArray.splice(firstArray.indexOf(item), 1)
+
+        }
+    }
+
+    finalArray = secondArray.concat(firstArray)
+
+    return finalArray
+
+}
+
+module.exports = subtractStringArrays
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const subtractStringArrays = require("./features/arrays/subtractStringArrays")
 const getPrimeFactors = require("./features/numbers/getPrimeFactors")
 const isEvenNumber = require("./features/numbers/isEvenNumber")
 
 module.exports = {
     isEvenNumber: isEvenNumber,
-    getPrimeFactors: getPrimeFactors
+    getPrimeFactors: getPrimeFactors,
+    subtractStringArrays: subtractStringArrays
 }
